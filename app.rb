c883110ec19ea7c40c661f385f321ffa50e04a6f
Andrea Reginato <andrea.reginato@gmail.com>
2013-05-02 10:21:43 +0200

    Clean up after moving demo to a standalone app

--- app.rb
+++ app.rb
@@ -3,19 +3,7 @@ require 'sinatra/base'
 class App < Sinatra::Base
   set :public_folder, File.dirname(__FILE__) + '/dist'
 
-  get '/index-dev-local.html/*', provides: 'html' do
-    send_file File.join(settings.public_folder, 'index-dev-local.html')
-  end
-
-  get '/index-dev.html/*', provides: 'html' do
-    send_file File.join(settings.public_folder, 'index-dev.html')
-  end
-
-  get '/demo/*', provides: 'html' do
-    send_file File.join(settings.public_folder, 'demo.html')
-  end
-
-  get '/*', provides: 'html' do
+  get '/*' do
     send_file File.join(settings.public_folder, 'index.html')
   end
 end
