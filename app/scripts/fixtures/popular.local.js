6971455720ed6967b0052d0da3afbdb673eaeb44
Andrea Reginato <andrea.reginato@gmail.com>
2013-05-09 14:20:59 +0200

    Added better structure for popular types structure

---
+++ app/scripts/fixtures/popular.local.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var popularTypes =
+[{
+	"id": "5176538ed033a9ae07000002",
+	"uri": "http://localhost:8000/types/5176538ed033a9ae07000002",
+	"name": "Connected Light",
+	"categories": ["lights"],
+	"owner": {
+		"id": "50ab8545d033a9810a000001",
+		"uri": "http://localhost:8000/people/50ab8545d033a9810a000001"
+	},
+	"description": "**[Local]** This is the most basic connected light.",
+	"created_at": "2013-04-23T09:25:34Z",
+	"updated_at": "2013-05-08T10:20:20Z"
+}];
