117f662d10872b0dc7c973c66ca55ff537d01786
Andrea Reginato <andrea.reginato@gmail.com>
2014-07-15 15:35:28 +0200

    Built devices from commit 5b8f11c on branch master

---
+++ scripts/258bac1b.config.js
@@ -0,0 +1,16 @@
+"use strict";
+
+ angular.module("config", [])
+
+.constant("ENV", {
+  "name": "production",
+  "endpoint": "http://api.lelylan.com",
+  "credentials": {
+    "site": "http://people.lelylan.com",
+    "clientId": "3bfdab6de9b9f2b82c595bd8befef178d5ea929dc40b0848de6a67b2a182d709",
+    "redirectUri": "http://lelylan.github.io/devices-dashboard-ng",
+    "profileUri": "http://api.lelylan.com/me"
+  }
+})
+
+;
